e55e6237e0b8cb17a1778243f92b10f610cffe82
appdeployments <54120773+appdeployments@users.noreply.github.com>
2019-08-19 08:30:58 -0400

    Add files via upload

---
+++ precache-manifest.14af8cc1ed2c71943111183fee97948e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0e93820441bcbbcbc522f9d84376165",
+    "url": "/shuffling2/index.html"
+  },
+  {
+    "revision": "ffaff1f6396dc20f992c",
+    "url": "/shuffling2/static/css/main.89ffe34c.chunk.css"
+  },
+  {
+    "revision": "c79f2ab33b1afe0fe496",
+    "url": "/shuffling2/static/js/2.6d328d6d.chunk.js"
+  },
+  {
+    "revision": "ffaff1f6396dc20f992c",
+    "url": "/shuffling2/static/js/main.1c7ce236.chunk.js"
+  },
+  {
+    "revision": "a130ee94af171e8982d5",
+    "url": "/shuffling2/static/js/runtime~main.a8a8fa16.js"
+  }
+]);
